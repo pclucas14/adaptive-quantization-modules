5ef370a74cd8f2141fc230ae22196d70555368d1
pclucas14 <lucas.page-caccia@mail.mcgill.ca>
2019-09-12 19:29:59 -0400

    more general; supports hierarchical buffer

--- buffer.py
+++ buffer.py
@@ -5,113 +5,92 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Buffer(nn.Module):
-    def __init__(self, args):
+    def __init__(self, input_size, n_classes, dtype=torch.LongTensor):
         super().__init__()
-        self.args   = args
-        buffer_size = args.mem_size
-        print('buffer has %d slots' % buffer_size)
 
-        #input_size = (args.layers[0].enc_height // args.layers[0].quant_size[0] ,) * 2
+        bx = dtype(0, *input_size).fill_(0)
+        by = torch.LongTensor(0).fill_(0)
+        bt = torch.LongTensor(0).fill_(0)
 
-        #bx = torch.LongTensor(buffer_size, *input_size).to(args.device).fill_(0)
-        bx = torch.FloatTensor(buffer_size, 2, 40, 512).to(args.device).fill_(0.)
-        by = torch.LongTensor(buffer_size).to(args.device).fill_(0)
-        bt = torch.LongTensor(buffer_size).to(args.device).fill_(0)
+        self.n_samples = 0
+        self.n_memory  = 0
+        self.mem_per_sample = np.prod(input_size)
 
-        self.current_index = 0
-        self.n_seen_so_far = 0
-
-        # registering as buffer allows us to save the object using `torch.save`
         self.register_buffer('bx', bx)
         self.register_buffer('by', by)
         self.register_buffer('bt', bt)
 
-        self.to_one_hot  = lambda x : x.new(x.size(0), args.n_classes).fill_(0).scatter_(1, x.unsqueeze(1), 1)
+        self.to_one_hot  = lambda x : x.new(x.size(0), n_classes).fill_(0).scatter_(1, x.unsqueeze(1), 1)
         self.arange_like = lambda x : torch.arange(x.size(0)).to(x.device)
         self.shuffle     = lambda x : x[torch.randperm(x.size(0))]
 
     @property
     def x(self):
-        return self.bx[:self.current_index]
+        return self.bx[:self.n_samples]
 
     @property
     def y(self):
-        return self.to_one_hot(self.by[:self.current_index])
+        return self.to_one_hot(self.by[:self.n_samples])
 
     @property
     def t(self):
-        return self.bt[:self.current_index]
+        return self.bt[:self.n_samples]
 
-    def display(self):
-        from torchvision.utils import save_image
-        from PIL import Image
+    def add(self, in_x, in_y, in_t, swap_idx):
+        """ concatenate a sample at the end of the buffer """
 
-        if 'cifar' in self.args.dataset:
-            shp = (-1, 3, 32, 32)
-        else:
-            shp = (-1, 3, 128, 128)
+        # convert int `in_t` to long tensor
+        in_t = torch.LongTensor(in_x.size(0)).to(in_x.device).fill_(in_t)
 
-        save_image((self.x.reshape(shp) * 0.5 + 0.5), 'tmp.png', nrow=int(self.current_index ** 0.5))
-        Image.open('tmp.png').show()
-        print(self.by[:self.current_index])
+        if self.bx.size(0) > in_x.size(0):
+            if swap_idx is None:
+                swap_idx = torch.randperm(self.bx.size(0))[:in_x.size(0)]
 
-    def add_reservoir(self, x, y, t):
-        n_elem = x.size(0)
+            tmp_x, tmp_y, tmp_t = self.bx[swap_idx], self.by[swap_idx], self.bt[swap_idx]
 
-        # add whatever still fits in the buffer
-        place_left = max(0, self.bx.size(0) - self.current_index)
-        if place_left:
-            offset = min(place_left, n_elem)
-            self.bx[self.current_index: self.current_index + offset].data.copy_(x[:offset])
-            self.by[self.current_index: self.current_index + offset].data.copy_(y[:offset])
-            self.bt[self.current_index: self.current_index + offset].fill_(t)
+            # overwrite
+            self.bx[swap_idx]   = in_x
+            self.by[swap_idx]   = in_y
+            self.bt[swap_idx]   = in_t
 
-            self.current_index += offset
-            self.n_seen_so_far += offset
+            in_x, in_y, in_t = tmp_x, tmp_y, tmp_t
 
-            # everything was added
-            if offset == x.size(0):
-                return
+        self.bx = torch.cat((self.bx, in_x))
+        self.by = torch.cat((self.by, in_y))
+        self.bt = torch.cat((self.bt, in_t))
 
-        # remove what is already in the buffer
-        x, y = x[place_left:], y[place_left:]
+        self.n_samples += in_x.size(0)
+        self.n_memory  += in_x.size(0) * self.mem_per_sample
 
-        indices = torch.FloatTensor(x.size(0)).to(x.device).uniform_(0, self.n_seen_so_far).long()
-        valid_indices = (indices < self.bx.size(0)).long()
 
-        idx_new_data = valid_indices.nonzero().squeeze(-1)
-        idx_buffer   = indices[idx_new_data]
+    def free(self, n_samples):
+        """ free buffer space. Assumes data is shuffled when added"""
 
-        # perform overwrite op
-        self.bx[idx_buffer] = x[idx_new_data]
-        self.by[idx_buffer] = y[idx_new_data]
-        self.bt[idx_buffer] = t
+        n_samples = int(n_samples)
+        import pdb
+        assert n_samples <= self.n_samples, pdb.set_trace()
 
-        self.n_seen_so_far += x.size(0)
+        self.bx = self.bx[:-n_samples]
+        self.by = self.by[:-n_samples]
+        self.bt = self.bt[:-n_samples]
+
+        self.n_samples -= n_samples
+        self.n_memory  -= n_samples * self.mem_per_sample
+
 
     def sample(self, amt, exclude_task=None):
+
+        amt = int(amt)
         if exclude_task is not None:
             valid_indices = (self.t != exclude_task).nonzero().squeeze()
             bx, by = self.bx[valid_indices], self.by[valid_indices]
         else:
-            bx, by = self.bx[:self.current_index], self.by[:self.current_index]
+            bx, by = self.bx[:self.n_samples], self.by[:self.n_samples]
 
         if bx.size(0) < amt:
-            # return self.bx[:self.current_index], self.by[:self.current_index]
+            # return self.bx[:self.n_samples], self.by[:self.n_samples]
             return bx, by
         else:
-            indices = torch.from_numpy(np.random.choice(bx.size(0), amt, replace=False)).to(self.args.device)
+            indices = torch.from_numpy(np.random.choice(bx.size(0), amt, replace=False)).to(bx.device)
             return bx[indices], by[indices]
-
-    def sample_from_task(self, amt, task):
-        valid_indices = (self.t == task).nonzero().squeeze()
-        bx, by = self.bx[valid_indices], self.by[valid_indices]
-        indices = torch.from_numpy(np.random.choice(bx.size(0), amt, replace=False)).to(self.args.device)
-        return bx[indices], by[indices]
-
-
-    def split(self, amt):
-        indices = torch.randperm(self.current_index).to(self.args.device)
-        return indices[:amt], indices[amt:]
-
 
